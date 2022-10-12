406183a0fa5656d9c06ebc9568ca91388a415bd9
seffipeguy <seffipeguy@gmail.com>
2022-10-12 17:13:41 +0100

    First commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: true,
+  app_name: "Grand QUIZ SabiFoot",
+  api_url: "https://qzbo.consonaute.biz/foot/api/",
+  base_link: "https://jeu.archivesdafrique.com/quiz?p=",
+  key_token: "access_token",
+  key_user_data: "user_data",
+  key_lang_data: "lang_data",
+  recaptcha_site_key: "6LdV3NYZAAAAAPiJe0Byyu2-R-2kaLDyDF06SKoW",
+  recaptcha_key: "6LeYMNsZAAAAAO7JRdIEpbOvtcyDYj4YQK47tmKb",
+  firebase: {
+    apiKey: 'AIzaSyA0z97vEnmB6ws96JjNr9_2Ui2BbFkYZ3E',
+    authDomain: 'sabi-foot-quiz.firebaseapp.com',
+    projectId: 'sabi-foot-quiz',
+    storageBucket: 'sabi-foot-quiz.appspot.com',
+    messagingSenderId: '278178852237',
+    appId: '1:278178852237:web:611138ca54b0de5c91e1c4',
+    measurementId: 'G-YJQGWMP18M'
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,33 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  app_name: "Grand QUIZ SabiFOOT",
+  api_url: "https://qzbo.consonaute.biz/foot/api/",
+  base_link: "https://foot.archivesdafrique.com/quiz",
+  key_token: "access_token",
+  key_user_data: "user_data",
+  key_lang_data: "lang_data",
+  recaptcha_site_key: "6LdV3NYZAAAAAPiJe0Byyu2-R-2kaLDyDF06SKoW",
+  recaptcha_key: "6LeYMNsZAAAAAO7JRdIEpbOvtcyDYj4YQK47tmKb",
+  firebase: {
+    apiKey: 'AIzaSyA0z97vEnmB6ws96JjNr9_2Ui2BbFkYZ3E',
+    authDomain: 'sabi-foot-quiz.firebaseapp.com',
+    projectId: 'sabi-foot-quiz',
+    storageBucket: 'sabi-foot-quiz.appspot.com',
+    messagingSenderId: '278178852237',
+    appId: '1:278178852237:web:611138ca54b0de5c91e1c4',
+    measurementId: 'G-YJQGWMP18M'
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
